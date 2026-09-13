90180f5cd992b031cfd53560b4a7a521e77138d0
naman-jindal-personal <namanjindal113@gmail.com>
2026-09-13 17:10:53 +0530

    js complete, ts setup

---
+++ Week-16-ts/dist/intro-2.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=intro-2.d.ts.map
---
+++ Week-16-ts/dist/intro-2.js
@@ -0,0 +1,9 @@
+function addTwenty(num) {
+    return num + 20;
+}
+const ab = addTwenty(10);
+const ba = addTwenty(10.4);
+console.log(ab);
+console.log(ba);
+export {};
+//# sourceMappingURL=intro-2.js.map
